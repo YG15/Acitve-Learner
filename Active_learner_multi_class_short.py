983b70cb0934189b15756781032af8708609edd5
YG15 <YG15@users.noreply.github.com>
2018-01-14 09:20:27 +0200

    Add files via upload

--- Active_learner_multi_class_short.py
+++ Active_learner_multi_class_short.py
@@ -1,119 +1,204 @@
-################################################
-# Name: Multinominal Naive Bayes
-# Author: Yonathna Guttel & Omri Allouche
-# Purpose: Run a Multinominal Naive Bayes
-#          algorithms on labeled text which belongs to different classes
-# Arguments: A corpus (or Corpora) in a single file, can be changed but
-#            than the upload and preprocessing method should be corrected to match the new data
-# Returning: Prediction labels vector and accuracy measurement
-# date: 14.01.2018
-# Version: 2
-##############################################
+###############Active_learner_multi_class############
+# Authors: Omri Allouche  & Yonathan Guttel
+# Date: 01.01.2018
+# Description: Draft work for Text active learner
+#####################################################
+
+
 """
-Future plans:
-The Script should be organized in several levels:
-1. Most of the function should be gathered under a new class beloging to the mulitnomial NB classifier
-2. Improvement of the code complexity should be done in order to decrease it and reduce the running time of the algorithm
+~~~~~~~~~~~~~~~~~~~~~~~~~Work Flow~~~~~~~~~~~~~~~~~~~~~
+1. Upload data as panda's data frame (df)
+2. Upload previous model (if such exist)
+3. Save df and model as "ActiveLearner" class and set parameters
+4.(A) Run function "oracle_query" to label the samples in the df and update the model
+    ->"oracle_query" - use the following functions:
+    5.  "ActiveLearner.get_next_batch_for_labeling" - A method which find the samples with the lowest scores in the df
+        and output them as a batch
+        ->"ActiveLearner.get_next_batch_for_labeling" - use the following functions:
+        6. "ActiveLearner.predict" - Iterates over the df with the model in order to improve the model and
+            predict the prob of the df
+            -> "ActiveLearner.predict" - use the following functions:
+            7."ActiveLearner.fit"- a method which fit the model to the new X and Y
+        8."get_data_for_model" - Receives a df and clean it from the column predictions and predictions_prob
+            columns and returns it as X and y
+9.(B) Test the current accuracy of the model using the function "run_performance_analysis"
+    ->"run_performance_analysis" use the following functions:
+    10. "pr_auc" - display an PR AUC graph and its metrics
+    11. "roc_auc" - display an ROC AUC graph and its metrics
+12.(C) Run the method ActiveLeraner.exit_or_save to let the oracle decide if to stop and/or save the model and or to continue to label
+13. if needed, continue to iterate over steps A-C as needed.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
 """
 
 import pandas as pd
 import numpy as np
-import os
-from copy import deepcopy
-from sklearn.metrics import accuracy_score
-from nltk.tokenize import word_tokenize
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import label_binarize
-#from nltk.stem.porter import PorterStemmer
-from collections import defaultdict
-from collections import Counter
-from itertools import compress
-
-def get_data(file_name, binerize=False):
-    
-    #The functions upload a raw text file in which each line is a text sample, where the first line is tit class,
-    #separated from the text which triple blnk spaces. the function upload it as a pandas df, with two columns;
-    #"labels" and text". If the samples are kept in another format the proper changes should be done in the code,
-    #in order to ensure a working script
-
-    df=pd.read_csv(file_name, delimiter="\t", header=None, names=["label", "text"])
-    target = df['label']
-    if binerize == True: #in case we want to binarize results
-        classes = np.unique(target)
-        target = label_binarize(target, classes=classes)
-    data = df.iloc[:,-1]
-    return (data, target)
-
-def tokenize (text):
-    tokened_text = [word_tokenize(x) for x in text]
-    #stemmer = PorterStemmer()
-    #stemmed_text = [stemmer.stem(x).lower()) for x in tokened_text]  #Is it needed?
-    return(tokened_text) # return(stemmed_text)
-
-def priors(labels): #set the priors of the classes for the MNB classifier
-    classes_names = np.unique(labels)
-    n_class = len(classes_names)
-    priors={}
-    for c in classes_names:
-        priors[c]= (Counter(labels)[c])/n_class
-    return(priors)
-
-def likelihoods (tokened_text, labels,smoothing_val=1, test_text=[]): #tokens probabilities
-    classes_names = np.unique(labels)
-    classes_data={}
-    likelihood = defaultdict(dict)
-    combined_text=tokened_text+test_text # This is done to prevent a bug in which during prediction step a show up in the test a word which does not have a measured probability
-    voc_words = set([item for sublist in combined_text for item in sublist])
-    voc_size = len(voc_words)
-    for c in classes_names:
-        classes_data[c]=list(compress(tokened_text, list(labels==c)))
-        flat_list = [item for sublist in classes_data[c] for item in sublist]
-        flat_list_length = len(flat_list)
-        for w in voc_words:
-            likelihood[c][w] = (flat_list.count(w)+smoothing_val)/(flat_list_length+voc_size) #claculating probability for each words in each class
-    return (likelihood)
-
-def MNB_train(text,labels,smoothing_val, test_text=[]):
-    tokened_text = tokenize(text)
-    prior = priors(labels)
-    likelihood = likelihoods(tokened_text, labels,smoothing_val=smoothing_val, test_text=test_text)
-    return (prior, likelihood)
-
-def MNB_predict (text,labels,test, raw=True, smoothing_val=1):
-    class_names = np.unique(labels)
-    final_labels=[]
-    prop_test = deepcopy(test)
-    if raw==True:
-        prop_test =tokenize (test)
-    prior, likelihood= MNB_train(text, labels, smoothing_val,test_text=prop_test)
-    for t in prop_test:
-        class_scores = {}
-        for c in class_names:
-            class_scores[c]=prior[c]*np.prod(np.array([likelihood[c][w] for w in t]))
-        final_labels.append(max(class_scores, key=class_scores.get))
-    return (final_labels)
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import average_precision_score
+from sklearn.metrics import roc_curve, auc
 
 
+def get_data_for_model(df, true_label_col='true_label'):
+    labels=np.unique(df[true_label_col]) #get uniques labels
+    pred_prob_col= [str("prediction_proba_"+x) for x in labels] #create a vector of col names to store each label probability
+    cols_to_remove = set(['predictions', pred_prob_col, true_label_col])
+    cols_to_keep = [c for c in df.columns if c not in cols_to_remove]
+    X = df[cols_to_keep]
+    if true_label_col in df.columns:
+        y = df[true_label_col]
+    else:
+        y = None
 
-# Test our results
-if __name__ == '__main__':
-    # Set the working directory
-    path = "F:\Guttel\Desktop"
-    os.chdir(path)
+    return (X, y)
 
-    # Set parameters
-    alpha=1
-    test_p=0.2
+def oracle_query (df, true_label_col='true_label'): #df is the batch received from method "get_next_batch"
+    current_batch=get_next_batch_for_labeling(df, batch_size=10) #estract the x samples with the lowest pred_prob values
+    for i in range(batch_size):
+        print ("Please type the correct label of the following sample: ", end="")
+        print (current_batch[i])
+        oracle_answer=input("Label: ")
+        print ("Your answer is ", oracle_answer, "do you wish to proceed or to correct yor answer?") #verify that the right answer was inputed
+        correct_answer = input ("""If label is correct type "Y" if you wish to correct it type "N" """)
+        while not correct_answer in ["Y","y","N","n"]:
+            correct_answer = input("""Please answer again: if label is correct type "Y", if you wish to correct it type "N" """)
+        if correct_answer in ["N","n"]:
+            oracle_answer = input("Correct Label: ")
 
-    # Choose the corpus on which the model will run
-    file_name = 'SMSSpamCollection.txt'
+        current_batch[i][true_label_col]=oracle_answer # enter the input to the right label column
 
-    # Preprocess it for the NB
-    X, y = get_data(file_name)
-    test_sep_ind=round(len(y)*test_p)
-    
-    # present results
-    MNB_pred = MNB_predict (X[test_sep_ind:],y[test_sep_ind:],X[:test_sep_ind], raw=True, smoothing_val=alpha)
-    true = y[:test_sep_ind]
+    df = df.combine_first(current_batch) #merge the results of new trur label back to the original df
 
-    MNB_accuracy_score = accuracy_score(true,MNB_pred)
-    print("Multinomial accuracy score is","{0:.4f}".format(MNB_accuracy_score))
+    return (df)
+
+def pr_auc(y_true, y_pred, n_classes):
+
+    #Compute precision and recall and PR area for multi class For multi class, should work fine with single class cases
+    precision = dict()
+    recall = dict()
+    average_precision = dict()
+    for i in range(n_classes):
+        precision[i], recall[i], _ = precision_recall_curve(y_true[:, i], y_pred[:, i])
+        average_precision[i] = average_precision_score(y_true[:, i], y_pred[:, i])
+
+    # A "micro-average": quantifying score on all classes jointly
+    precision["micro"], recall["micro"], _ = precision_recall_curve(y_true.ravel(), y_pred.ravel())
+    average_precision["micro"] = average_precision_score(y_true, y_pred, average="micro")
+
+    # Plot graph and display value
+    plt.figure()
+    plt.step(recall['micro'], precision['micro'], color='b', alpha=0.2, where='post')
+    plt.fill_between(recall["micro"], precision["micro"], step='post', alpha=0.2, color='b')
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('Average precision score, micro-averaged over all classes: AP={0:0.2f}'
+            .format(average_precision["micro"]))
+
+    return ()
+
+def roc_auc(y_true, y_pred, n_classes):
+    # Compute ROC curve and ROC area for multi class, should work fine with single class cases
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_true[:, i], y_pred[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_true.ravel(), y_pred.ravel())
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+    # Plot graph and display value
+    plt.figure()
+    lw = 2
+    plt.plot(fpr["micro"], tpr["micro"], color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc["micro"])
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
+
+    return ()
+
+def run_performance_analysis (df, true_label_col='true_label'):
+    org_df = df.copy()
+    df_sub = org_df[org_df[true_label_col] != None] # keep only sample which have true label
+    labels = np.unique(df_sub[true_label_col])
+    pred_prob_col = [str("prediction_proba_" + x) for x in labels] #see above
+    Y_pred = df_sub[pred_prob_col]  # All the different label prediction probabilities columns
+    y_true = df_sub[true_label_col] # The labels column
+    # Use label_binarize to be multi-label like settings
+    Y_true = label_binarize(y_true, classes=labels) #binarize Labels column
+    n_classes = len(labels)
+
+    pr_auc(Y_true, Y_pred, n_classes)
+    roc_auc(Y_true, Y_pred, n_classes)
+
+    return ()
+
+
+class ActiveLearner:
+    def __init__(self,
+                 model,
+                 bootstrap_iterations = 0,
+                 th_prediction_confidence = 0.8
+                ):
+        self.bootstrap_iterations = bootstrap_iterations
+        self.th_prediction_confidence = th_prediction_confidence
+        self.model = model
+
+    def predict(self, df):
+        model = self.model
+        X, _ = get_data_for_model(df)
+        Y_pred = model.predict_proba(X)
+
+        if self.bootstrap_iterations > 0:
+            org_df = df.copy()
+
+            for i in range(self.bootstrap_iterations):
+                df = org_df[Y_pred > self.th_prediction_confidence]
+                X, _ = get_data_for_model(df)
+                model.fit(X, Y_pred)
+                Y_pred = model.predict_proba(org_df)
+
+            df = org_df
+
+        labels = np.unique(df[true_label_col])
+        pred_prob_col = [str("prediction_proba_" + x) for x in labels]
+
+        df['predictions'] = np.argmax(Y_pred, axis=1)
+        for col, i in zip(pred_prob_col, range(len(labels))):
+            df[col]= Y_pred[i]
+
+        return (df)
+
+    def fit(self, df):
+        X, y = get_data_for_model(df)
+        self.model.fit(X, y)
+        return self
+
+    def get_next_batch_for_labeling(self, df, batch_size=10):
+        X, _ = get_data_for_model(df)
+        df = self.predict(X)
+        sorted = df.sort_values('predictions_proba')  # sort rows by confidence
+        return sorted.head(batch_size)
+
+    def exit_and_save(self, df):
+        save_answer = input("""Do you wish to save the process? If yes type "Y" """)
+        exit_answer=input("""Do you wish to stop? If yes type "Y" """)
+        if exit_answer in ["Y","y"] and save_answer in ["Y","y"]:
+            #save df and self/model and exit
+        elif save_answer in ["Y","y"]:
+            # save df and self/model  and proceed
+        elif exit_answer in ["Y","y"]:
+            # exit without saving
+        else
+            pass
+    return (self, df)
--- NB_yonathan.py
+++ NB_yonathan.py
@@ -1,81 +1,124 @@
 ################################################
-# Name: Bernoulli and Multinominal Naive Bayes
+# Name: Multinominal Naive Bayes
 # Author: Yonathna Guttel & Omri Allouche
-# Purpose: Run a Bernoulli or/and a Multinominal Naive Bayes
+# Purpose: Run a Multinominal Naive Bayes
 #          algorithms on labeled text which belongs to different classes
 # Arguments: A corpus (or Corpora) in a single file, can be changed but
 #            than the upload and preprocessing method should be corrected to match the new data
 # Returning: Prediction labels vector and accuracy measurement
-# date: 09.01.2018
-# Version: 1
+# date: 14.01.2018
+# Version: 2
 ##############################################
+"""
+Future plans:
+The Script should be organized in several levels:
+1. Most of the function should be gathered under a new class beloging to the mulitnomial NB classifier
+2. Improvement of the code complexity should be done in order to decrease it and reduce the running time of the algorithm
+"""
 
 import pandas as pd
 import numpy as np
 import os
 from copy import deepcopy
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.naive_bayes import BernoulliNB,MultinomialNB
+from sklearn.metrics import accuracy_score
+from nltk.tokenize import word_tokenize
 from sklearn.preprocessing import label_binarize
+# from nltk.stem.porter import PorterStemmer
+from collections import defaultdict
+from collections import Counter
+from itertools import compress
 
-#set the working directory
-path= "F:\Guttel\Desktop"
-os.chdir(path)
 
-def orgenize_data(data):
-    """
-    The functions takes a df/array of a raw texts and transform
-    them into a tf-idf objects array which can use as the x for a NB algorithm
-    """
-    raw_df= deepcopy(data) # copy data
-    count_vect = CountVectorizer()
-    X_train_counts = count_vect.fit_transform(raw_df) #Text preprocessing, tokenizing and filtering of stopwords - we build a dictionary of features and transform documents to feature vectors
-    tfidf_transformer = TfidfTransformer()
-    X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts) # Calculating "Term Frequency times Inverse Document Frequency" (tf=idf)
-    return (X_train_tfidf)
+def get_data(file_name, binerize=False):
+    # The functions upload a raw text file in which each line is a text sample, where the first line is tit class,
+    # separated from the text which triple blnk spaces. the function upload it as a pandas df, with two columns;
+    # "labels" and text". If the samples are kept in another format the proper changes should be done in the code,
+    # in order to ensure a working script
 
-def get_data(file_name):
-    """
-    The functions upload a raw text file in which each line is a text sample, where the first line is tit class,
-    separated from the text which triple blnk spaces. the function upload it as a pandas df, with two columns;
-    "labels" and text". If the samples are kept in another format the proper changes should be done in the code,
-    in order to ensure a working script
-    """
-    df=pd.read_csv(file_name, delimiter="\t", header=None, names=["label", "text"])
-    data = orgenize_data(df['text'])
-    """
-    The next two lines are in case we want to binarize results
-    """
-    #classes =np.unique(df['label'])
-    #target = label_binarize(df['label'], classes=classes)
+    df = pd.read_csv(file_name, delimiter="\t", header=None, names=["label", "text"])
     target = df['label']
-    return data, target
+    if binerize == True:  # in case we want to binarize results
+        classes = np.unique(target)
+        target = label_binarize(target, classes=classes)
+    data = df.iloc[:, -1]
+    return (data, target)
+
+
+def tokenize(text):
+    tokened_text = [word_tokenize(x) for x in text]
+    # stemmer = PorterStemmer()
+    # stemmed_text = [stemmer.stem(x).lower()) for x in tokened_text]  #Is it needed?
+    return (tokened_text)  # return(stemmed_text)
+
+
+def priors(labels):  # set the priors of the classes for the MNB classifier
+    classes_names = np.unique(labels)
+    n_class = len(classes_names)
+    priors = {}
+    for c in classes_names:
+        priors[c] = (Counter(labels)[c]) / n_class
+    return (priors)
+
+
+def likelihoods(tokened_text, labels, smoothing_val=1, test_text=[]):  # tokens probabilities
+    classes_names = np.unique(labels)
+    classes_data = {}
+    likelihood = defaultdict(dict)
+    combined_text = tokened_text + test_text  # This is done to prevent a bug in which during prediction step a show up in the test a word which does not have a measured probability
+    voc_words = set([item for sublist in combined_text for item in sublist])
+    voc_size = len(voc_words)
+    for c in classes_names:
+        classes_data[c] = list(compress(tokened_text, list(labels == c)))
+        flat_list = [item for sublist in classes_data[c] for item in sublist]
+        flat_list_length = len(flat_list)
+        for w in voc_words:
+            likelihood[c][w] = (flat_list.count(w) + smoothing_val) / (
+                    flat_list_length + voc_size)  # claculating probability for each words in each class
+    return (likelihood)
+
+
+def MNB_train(text, labels, smoothing_val, test_text=[]):
+    tokened_text = tokenize(text)
+    prior = priors(labels)
+    likelihood = likelihoods(tokened_text, labels, smoothing_val=smoothing_val, test_text=test_text)
+    return (prior, likelihood)
+
+
+def MNB_predict(text, labels, test, raw=True, smoothing_val=1):
+    class_names = np.unique(labels)
+    final_labels = []
+    prop_test = deepcopy(test)
+    if raw == True:
+        prop_test = tokenize(test)
+    prior, likelihood = MNB_train(text, labels, smoothing_val, test_text=prop_test)
+    for t in prop_test:
+        class_scores = {}
+        for c in class_names:
+            class_scores[c] = prior[c] * np.prod(np.array([likelihood[c][w] for w in t]))
+        final_labels.append(max(class_scores, key=class_scores.get))
+    return (final_labels)
+
 
 # Test our results
 if __name__ == '__main__':
+    # Set the working directory
+    path = "F:\Guttel\Desktop"
+    os.chdir(path)
 
-    #Choose the corpus on which the model will run
+    # Set parameters
+    alpha = 1
+    test_p = 0.2
+
+    # Choose the corpus on which the model will run
     file_name = 'SMSSpamCollection.txt'
 
     # Preprocess it for the NB
     X, y = get_data(file_name)
+    test_sep_ind = round(len(y) * test_p)
 
-    #Set the classifier and fit it to thw data
-    #Here is the place for later stages to set the classifiers parapmeters
-    MNB_clf = MultinomialNB()
-    BNB_clf = BernoulliNB()
+    # present results
+    MNB_pred = MNB_predict(X[test_sep_ind:], y[test_sep_ind:], X[:test_sep_ind], raw=True, smoothing_val=alpha)
+    true = y[:test_sep_ind]
 
-    #fir models
-    MNB_clf.fit(X[100:], y[100:])
-    BNB_clf.fit(X[100:], y[100:])
-
-    #present results
-    MNB_pred = (MNB_clf.predict(X[:100]))
-    BNB_pred = (BNB_clf.predict(X[:100]))
-    true = y[:100]
-
-    MNB_accuracy_score = MNB_clf.score(X[:100],y[:100])
-    BNB_accuracy_score = BNB_clf.score(X[:100],y[:100])
-    print("Multinomial accuracy score is","{0:.4f}".format(MNB_accuracy_score))
-    print("Bernoulli accuracy score is","{0:.4f}".format(BNB_accuracy_score))
+    MNB_accuracy_score = accuracy_score(true, MNB_pred)
+    print("Multinomial accuracy score is", "{0:.4f}".format(MNB_accuracy_score))
